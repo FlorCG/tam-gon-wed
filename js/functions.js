980abeff7af047a61a5dbbb43b0fd32a66ed2483
unknown <flor.chovanec@hotmail.com>
2020-03-09 14:57:06 -0300

    ultimos detalles agregados

--- js/functions.js
+++ js/functions.js
@@ -49,3 +49,11 @@ document.addEventListener('DOMContentLoaded', function() {
   $(document).ready(function(){
     $('.slider').slider();
   });
+
+
+$(window).on('load', function () {
+      setTimeout(function () {
+    $(".loader").css({visibility:"hidden",opacity:"0"})
+  }, 2000);
+     
+});
