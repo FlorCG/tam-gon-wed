efc85ab5b158d56c7c81cd1304d1546d729c462f
unknown <flor.chovanec@hotmail.com>
2020-03-09 01:06:51 -0300

    modificacion de portada

--- js/functions.js
+++ js/functions.js
@@ -39,3 +39,13 @@ if ($('#countdown').length) {
 
 
 
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.slider').slider();
+  });
